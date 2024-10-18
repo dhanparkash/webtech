c3e5e571dec13f254e01d2b22975fa08db030c3e
dhanparkash <114330505+dhanparkash@users.noreply.github.com>
2024-10-18 16:02:01 +0530

    add two pages about us and contact us page

---
+++ src/app/about-us/bio/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Bio</div>
+  )
+}
+
+export default Page
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-between px-11 py-1">
+    <div><Image src='/logotech.jpg' width={200} height={70} alt="logo"/></div>
+    <div>
+      <nav>
+          <ul className="flex items-center gap-10">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact</li>
+              <li>Clogs</li>
+          </ul>
+      </nav>
+    </div>
+  </div>
+  )
+}
+
+export default Footer
