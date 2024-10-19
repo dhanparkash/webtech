b53e1c28c00c3ec71635394fe21be68fe18cf081
dhanparkash <114330505+dhanparkash@users.noreply.github.com>
2024-10-19 16:25:27 +0530

    solved error

--- src/components/home-page/cientsay.jsx
+++ src/components/home-page/cientsay.jsx
@@ -20,7 +20,7 @@ const Clientsay = () => {
 	<div className="container max-w-xl mx-auto">
 		<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-50 dark:text-gray-800">
 			<img src="/clientsay.webp" alt="" className="w-20 h-20 rounded-full dark:bg-gray-500" />
-			<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+			<blockquote className="max-w-lg text-lg italic font-medium text-center">Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!</blockquote>
 			<div className="text-center dark:text-gray-600">
 				<p>Leroy Jenkins</p>
 				<p>CEO of Company Co.</p>
@@ -30,7 +30,7 @@ const Clientsay = () => {
 	<div className="container max-w-xl mx-auto">
 		<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-50 dark:text-gray-800">
 			<img src="/clientsay.webp" alt="" className="w-20 h-20 rounded-full dark:bg-gray-500" />
-			<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+			<blockquote className="max-w-lg text-lg italic font-medium text-center">Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!</blockquote>
 			<div className="text-center dark:text-gray-600">
 				<p>Leroy Jenkins</p>
 				<p>CEO of Company Co.</p>
@@ -40,7 +40,7 @@ const Clientsay = () => {
 	<div className="container max-w-xl mx-auto">
 		<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-50 dark:text-gray-800">
 			<img src="/clientsay.webp" alt="" className="w-20 h-20 rounded-full dark:bg-gray-500" />
-			<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+			<blockquote className="max-w-lg text-lg italic font-medium text-center">Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!</blockquote>
 			<div className="text-center dark:text-gray-600">
 				<p>Leroy Jenkins</p>
 				<p>CEO of Company Co.</p>
@@ -50,7 +50,7 @@ const Clientsay = () => {
 	<div className="container max-w-xl mx-auto">
 		<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-50 dark:text-gray-800">
 			<img src="/clientsay.webp" alt="" className="w-20 h-20 rounded-full dark:bg-gray-500" />
-			<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+			<blockquote className="max-w-lg text-lg italic font-medium text-center">Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!</blockquote>
 			<div className="text-center dark:text-gray-600">
 				<p>Leroy Jenkins</p>
 				<p>CEO of Company Co.</p>
