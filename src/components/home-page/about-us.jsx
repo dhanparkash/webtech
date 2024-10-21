8af601027967b3f783b00e3106b26460f5da630b
dhanparkash <dhanparkashdhiman7@gmail.com>
2024-10-21 22:43:31 +0530

    flex change to grid in about us section

--- src/components/home-page/about-us.jsx
+++ src/components/home-page/about-us.jsx
@@ -27,21 +27,21 @@ const Whychooseus = () => {
                             </div>
                         </div>
                         <div className="w-full flex-col justify-center items-start gap-6 flex">
-                            <div className="w-full md:justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
+                            <div className="w-full h-full grid grid-cols-2 gap-8">
                                 <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
                                     <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">8+ Years</h4>
                                     <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Influencing Digital
                                         Landscapes Together</p>
                                 </div>
-                                <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
+                            <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
                                     <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">125+ Projects
                                     </h4>
                                     <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Excellence Achieved
                                         Through Success</p>
                                 </div>
                             </div>
-                            <div className="w-full h-full md:justify-start items-center gap-8 grid md:grid-cols-2 sm:grid-cols-1">
-                                <div className="w-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
+                            <div className="w-full h-full grid grid-cols-2 gap-8">
+                                <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
                                     <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">26+ Awards</h4>
                                     <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Our Dedication to
                                         Innovation Wins Understanding</p>
@@ -56,7 +56,7 @@ const Whychooseus = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-full lg:justify-end justify-center items-start flex">
+                <div className="w-full lg:justify-end justify-center items-start flex m-2">
                     <div
                         className="sm:w-[564px] w-full sm:h-[646px] h-full sm:bg-gray-100 rounded-3xl sm:border border-gray-200 relative">
                         <img className="sm:mt-5 sm:ml-5 w-full h-full rounded-3xl object-cover"
