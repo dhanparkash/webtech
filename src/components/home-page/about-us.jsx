57aeede0e135b3c64a69377d6877302a2016774d
dhanparkash <114330505+dhanparkash@users.noreply.github.com>
2024-10-22 14:46:04 +0530

    pulling main bracnch

--- src/components/home-page/about-us.jsx
+++ src/components/home-page/about-us.jsx
@@ -1,13 +1,13 @@
 "use client"
 import React from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
 
 const Whychooseus = () => {
  
   return (
-    <div className='w-10/12 mx-auto py-10'>
+    <div className='xxl:w-10/12 md:w-11/12 sm:w-full mx-auto py-10'>
 	<h2 className="text-indigo-700 text-4xl font-bold font-manrope leading-normal text-center">About Us</h2>
     <section className="py-10 relative xl:mr-0 lg:mr-5 mr-0">
         <div className="w-full px-4 md:px-5 lg:px-5 mx-auto">
@@ -27,40 +27,36 @@ const Whychooseus = () => {
                             </div>
                         </div>
                         <div className="w-full flex-col justify-center items-start gap-6 flex">
-                            <div className="w-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9">33+ Years</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Influencing Digital
+                            <div className="w-full h-full grid md:grid-cols-2 gap-8">
+                                <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
+                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">8+ Years</h4>
+                                    <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Influencing Digital
                                         Landscapes Together</p>
                                 </div>
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9">125+ Projects
+                            <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
+                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">125+ Projects
                                     </h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Excellence Achieved
+                                    <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Excellence Achieved
                                         Through Success</p>
                                 </div>
                             </div>
-                            <div className="w-full h-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                                <div
-                                    className="w-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9">26+ Awards</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Our Dedication to
+                            <div className="w-full h-full grid md:grid-cols-2 gap-8">
+                                <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group">
+                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">26+ Awards</h4>
+                                    <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Our Dedication to
                                         Innovation Wins Understanding</p>
                                 </div>
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9">99% Happy
+                                <div className="w-full h-full p-3.5 rounded-xl border border-indigo-700 hover:border-gray-400 transition-all duration-400 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:bg-indigo-700 group-hover:text-white group">
+                                    <h4 className="text-gray-900 text-2xl font-bold font-manrope leading-9 group-hover:text-white">99% Happy
                                         Clients</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Mirrors our Focus on
+                                    <p className="text-gray-500 text-base font-normal leading-relaxed group-hover:text-white">Mirrors our Focus on
                                         Client Satisfaction.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-full lg:justify-end justify-center items-start flex">
+                <div className="w-full lg:justify-end justify-center items-start flex m-2">
                     <div
                         className="sm:w-[564px] w-full sm:h-[646px] h-full sm:bg-gray-100 rounded-3xl sm:border border-gray-200 relative">
                         <img className="sm:mt-5 sm:ml-5 w-full h-full rounded-3xl object-cover"
